90c9753dfd66b47a5aa21a47cf3f4b2dcdc4e88b
jmandrake <mandrakebiz@gmail.com>
2023-06-15 08:37:45 -0700

    Update lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,13 @@
-#testing
+import json
+import pandas as pd
+
+
+def lambda_handler(event, context):
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done v1.1')
+    return {
+        'statusCode': 200,
+        'body': 'Hello from Lambda!'
+    }
